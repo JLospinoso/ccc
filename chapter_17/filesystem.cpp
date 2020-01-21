c0d224d3879503b0981726aadca9d7d3d0c94077
Josh Lospinoso <josh@lospi.net>
2020-01-21 12:10:22 -0500

    Closes #117

--- chapter_17/filesystem.cpp
+++ chapter_17/filesystem.cpp
@@ -3,7 +3,7 @@
 #include <string>
 #include <filesystem>
 
-TEST_CASE("ostringstream produces strings with str") {
+TEST_CASE("std::filesystem::path supports == and .empty()") {
   std::filesystem::path empty_path;
   std::filesystem::path shadow_path{ "/etc/shadow" };
 
