75611c612c92f4050f178df5b3fdbb3871d76fdf
Josh Lospinoso <josh@lospi.net>
2020-01-21 15:12:13 -0500

    Closes #140

--- chapter_19/factor_serial.cpp
+++ chapter_19/factor_serial.cpp
@@ -2,7 +2,6 @@
 #include <chrono>
 #include <iostream>
 #include <limits>
-#include <set>
 #include <sstream>
 #include <string>
 #include <vector>
@@ -23,11 +22,11 @@ struct Stopwatch {
 };
 
 template <typename T>
-set<T> factorize(T x) {
-  set<T> result{ 1 };
+vector<T> factorize(T x) {
+  vector<T> result{ 1 };
   for(T candidate = 2; candidate <= x; candidate++) {
     if(x % candidate == 0) {
-      result.insert(candidate);
+      result.push_back(candidate);
       x /= candidate;
       candidate = 1;
     }
@@ -37,7 +36,7 @@ set<T> factorize(T x) {
 
 string factor_task(unsigned long long x) {
   chrono::nanoseconds elapsed_ns;
-  set<unsigned long long> factors;
+  vector<unsigned long long> factors;
   {
     Stopwatch stopwatch{ elapsed_ns };
     factors = factorize(x);
